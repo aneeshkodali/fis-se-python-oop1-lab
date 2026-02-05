a261cf1855b544763f67d67957575ece192aee6e
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-02-05 08:27:40 -0500

    Create initial book and coffee logic

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    
+
+    # init
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    # page_count - getter
+    def get_page_count(self):
+        return self._page_count
+    
+    # page_count - setter
+    def set_page_count(self, value):
+
+        # ensure valid value
+        if type(value) is int:
+            self._page_count = value
+
+        # otherwise print message indicating erroneous value
+        else:
+            print("page_count must be an integer")
+
+    # page_count - property
+    page_count = property(
+        fget=get_page_count,
+        fset=set_page_count
+    )
+
+    # page_count - turn page
+    def turn_page(self):
+
+        # increment page_count
+        self.page_count += 1
+
+        # print message
+        print("Flipping the page...wow, you read fast!")
+        
     
         
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+    # size - getter
+    def get_size(self):
+        return self._size
+    
+    # size - setter
+    def set_size(self, value):
+
+        # ensure valid value
+        if value in ('Small', 'Medium', 'Large'):
+            self._size = value
+        
+        # otherwise print message indicating erroneous value
+        else:
+            print("size must be Small, Medium, or Large")
+
+    # size - property
+    size = property(
+        fget=get_size,
+        fset=set_size
+    )
+
+    # price - tip
+    def tip(self):
+
+        # print message
+        print("This coffee is great, here’s a tip!")
+
+        # increment price
+        self.price += 1
